22d5486084dd7713e2de734b41a119598290d756
Pratishtha-06 <nandwalpratishtha06@gmail.com>
2025-03-27 23:43:44 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: "./", // Ensures correct asset paths
+  build: {
+    outDir: "dist"  // Ensures Vercel deploys from 'dist' folder
+  }
+})
